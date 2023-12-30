558b860b039d8f285cbc0eb18ff06f8a0dd27db4
Hy0tic <vothanhdat278@gmail.com>
2023-12-30 18:23:05 -0500

    add action to event

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	// "github.com/aws/aws-sdk-go/aws"
+    // "github.com/aws/aws-sdk-go/aws/session"
+    // "github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 )
 
 type MyEvent struct {
 	Name string `json:"name"`
 	Message string `json:"message"`
+	Action string `json:"action"`
 }
 
 func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
-	message := fmt.Sprintf("%s : %s", event.Name, event.Message)
+	message := fmt.Sprintf("%s : %s. Action: %s", event.Name, event.Message, event.Action)
 	return &message, nil
 }
 

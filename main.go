fa3bb4686a0550a8771ba31e7f9095fc77242bdf
Hy0tic <vothanhdat278@gmail.com>
2023-12-30 18:49:07 -0500

    get connectionID

--- main.go
+++ main.go
@@ -13,12 +13,19 @@ type MyEvent struct {
 	Name string `json:"name"`
 	Message string `json:"message"`
 	Action string `json:"action"`
+	RequestContext struct {
+        ConnectionID string `json:"connectionId"`
+    } `json:"requestContext"`
 }
 
 func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
+
+	connectionID := event.RequestContext.ConnectionID
+	fmt.Sprint(connectionID)
+
 	message := fmt.Sprintf("%s : %s. Action: %s", event.Name, event.Message, event.Action)
 	return &message, nil
 }

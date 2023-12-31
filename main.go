b446e4aa04e76e275293aa76d2fc6ef64cce17ea
Hy0tic <vothanhdat278@gmail.com>
2023-12-30 19:55:13 -0500

    lambda send message to api gateway

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
-	// "github.com/aws/aws-sdk-go/aws"
-    // "github.com/aws/aws-sdk-go/aws/session"
-    // "github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 )
 
 type MyEvent struct {
@@ -14,8 +14,10 @@ type MyEvent struct {
 	Message string `json:"message"`
 	Action string `json:"action"`
 	RequestContext struct {
-        ConnectionID string `json:"connectionId"`
-		RouteKey  string `json:"routeKey"`
+        ConnectionID	string `json:"connectionId"`
+		RouteKey 		string `json:"routeKey"`
+		DomainName   	string `json:"domainName"`
+        Stage        	string `json:"stage"`
     } `json:"requestContext"`
 }
 
@@ -25,25 +27,23 @@ type Response struct {
 }
 
 func HandleRequest(ctx context.Context, event *MyEvent) (Response, error) {
+	sess := session.Must(session.NewSession())
+	endpoint := "https://" + "o6xs157o67.execute-api.us-east-1.amazonaws.com/v1"
+	apiGwManagementApi := apigatewaymanagementapi.New(sess, aws.NewConfig().WithEndpoint(endpoint))
+	message2 := "Hello, react client!"
+
+    _, err := apiGwManagementApi.PostToConnection(&apigatewaymanagementapi.PostToConnectionInput{
+        ConnectionId: aws.String(event.RequestContext.ConnectionID),
+        Data:         []byte(message2),
+    })
+
     switch event.RequestContext.RouteKey {
     case "$connect":
-        // Handle connect
-		connectionID := event.RequestContext.ConnectionID
-		message := fmt.Sprint("%s connected",connectionID)
-		response := Response{
-			StatusCode: 200, // HTTP status code
-			Body:       message,
-		}
-		return response, nil
+		response, error := handle_connect(ctx, event)
+		return response, error
     case "$disconnect":
-        // Handle disconnect
-		connectionID := event.RequestContext.ConnectionID
-		message := fmt.Sprint("%s disconnected",connectionID)
-		response := Response{
-			StatusCode: 200, // HTTP status code
-			Body:       message,
-		}
-		return response, nil
+		response, error := handle_disconnect(ctx, event)
+		return response, error
     default:
         // Handle default message
     }
@@ -53,9 +53,31 @@ func HandleRequest(ctx context.Context, event *MyEvent) (Response, error) {
         StatusCode: 200, // HTTP status code
         Body:       message,
     }
-	return response, nil
+	return response, err
 }
 
 func main() {
 	lambda.Start(HandleRequest)
+}
+
+func handle_connect(ctx context.Context, event *MyEvent) (Response, error){
+	// Handle connect
+	connectionID := event.RequestContext.ConnectionID
+	message := fmt.Sprint("%s connected",connectionID)
+	response := Response{
+		StatusCode: 200, // HTTP status code
+		Body:       message,
+	}
+	return response, nil
+}
+
+func handle_disconnect(ctx context.Context, event *MyEvent) (Response, error){
+	// Handle connect
+	connectionID := event.RequestContext.ConnectionID
+	message := fmt.Sprint("%s disconnected",connectionID)
+	response := Response{
+		StatusCode: 200, // HTTP status code
+		Body:       message,
+	}
+	return response, nil
 }

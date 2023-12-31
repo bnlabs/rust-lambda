d86d22a305ad877be4c0d82ac2222e9f82f2c973
Hy0tic <vothanhdat278@gmail.com>
2023-12-30 20:25:53 -0500

    add policy for websocket lambda

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -45,13 +45,20 @@ resource "aws_iam_role_policy" "lambda_logging" {
   })
 }
 
-# resource "aws_lambda_permission" "api_gw_lambda" {
-#   statement_id  = "AllowExecutionFromAPIGateway"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.rust_lambda.function_name
-#   principal     = "apigateway.amazonaws.com"
+resource "aws_iam_role_policy" "lambda_websocket_policy" {
+  name = "lambda_websocket_policy"
+  role = aws_iam_role.rust_lambda_iam_role.id
 
-#   # Note: The source ARN is for the method within your API Gateway that triggers the Lambda.
-#   # Adjust the ARN as needed based on your API Gateway configuration.
-#   source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "execute-api:ManageConnections",
+        Resource = "arn:aws:execute-api:us-east-1:915898657279:o6xs157o67/v1/POST/@connections/*"
+      },
+    ],
+  })
+}
+
+

4092a47c057a3454b13dba0ca12d2e55a9961cec
Hy0tic <vothanhdat278@gmail.com>
2023-12-31 19:11:04 -0500

    write and delete connection ID

--- connect.go
+++ connect.go
@@ -3,15 +3,65 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
+type WebSocketConnection struct {
+    ConnectionID string `json:"ConnectionID" dynamodbav:"ConnectionID"`
+}
 
 func handle_connect(ctx context.Context, event *MyEvent) (Response, error){
 	// Handle connect
 	connectionID := event.RequestContext.ConnectionID
-	message := fmt.Sprintf("%s connected", connectionID)
+	message := fmt.Sprintf("%s connected \n", connectionID)
+
+	err := addConnectionIDToDynamoDB(ctx, event.RequestContext.ConnectionID)
+    if (err != nil){
+        fmt.Printf("ERROR ADDING ITEM TO DYNAMODB: %s\n", err)
+    }
 	response := Response{
 		StatusCode: 200, // HTTP status code
 		Body:       message,
 	}
-	return response, nil
+	return response, err
+}
+
+
+func addConnectionIDToDynamoDB(ctx context.Context, connectionID string) error {
+    // Create a new AWS session
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+    fmt.Print("Created new session")
+    // Create a DynamoDB client
+    svc := dynamodb.New(sess)
+    fmt.Print("Created new client")
+    // Create an instance of the WebSocketConnection struct
+    item := WebSocketConnection{
+        ConnectionID: connectionID,
+    }
+    fmt.Print("mapped item")
+    // Marshal the Go struct into a DynamoDB attribute value map
+    av, err := dynamodbattribute.MarshalMap(item)
+    if err != nil {
+        return fmt.Errorf("got error marshalling new item: %w", err)
+    }
+
+    // Define the PutItem input
+    input := &dynamodb.PutItemInput{
+        Item:      av,
+        TableName: aws.String("websocket-connectionIds"),
+    }
+
+    // Put the item into the DynamoDB table
+    _, err = svc.PutItemWithContext(ctx, input)
+    if err != nil {
+        return fmt.Errorf("failed to put item in DynamoDB: %w", err)
+    }
+
+    fmt.Printf("Successfully added connection ID %s to DynamoDB\n", connectionID)
+    return nil
 }
--- disconnect.go
+++ disconnect.go
@@ -3,15 +3,53 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 func handle_disconnect(ctx context.Context, event *MyEvent) (Response, error){
 	// Handle connect
 	connectionID := event.RequestContext.ConnectionID
 	message := fmt.Sprintf("%s connected", connectionID)
+
+	err := removeConnectionIDFromDynamoDB(ctx, event.RequestContext.ConnectionID)
+    if (err != nil){
+        fmt.Printf("ERROR ADDING ITEM TO DYNAMODB: %s\n", err)
+    }
+
 	response := Response{
 		StatusCode: 200, // HTTP status code
 		Body:       message,
 	}
 	return response, nil
+}
+
+func removeConnectionIDFromDynamoDB(ctx context.Context, connectionID string) error {
+    // Create a new AWS session
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+
+    // Create a DynamoDB client
+    svc := dynamodb.New(sess)
+
+    // Define the DeleteItem input
+    input := &dynamodb.DeleteItemInput{
+        TableName: aws.String("websocket-connectionIds"),
+        Key: map[string]*dynamodb.AttributeValue{
+            "ConnectionID": {
+                S: aws.String(connectionID),
+            },
+        },
+    }
+
+    // Delete the item from the DynamoDB table
+    _, err := svc.DeleteItemWithContext(ctx, input)
+    if err != nil {
+        return fmt.Errorf("failed to delete item from DynamoDB: %w", err)
+    }
+
+    fmt.Printf("Successfully removed connection ID %s from DynamoDB\n", connectionID)
+    return nil
 }
